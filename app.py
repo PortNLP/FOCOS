e9ee5d4b3dab8cf319db131bcf8d397bdadc38bc
mridenour7 <82553574+mridenour7@users.noreply.github.com>
2022-10-13 11:51:06 -0700

    Changed page name

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ app = Flask(__name__)
 model = model()
 
 """
-Function decorator === app.route('/',layout())
+Function decorator === app.route('/',exploration())
 """
 @app.route('/')
-@app.route('/layout.html')
-def layout():
+@app.route('/exploration.html')
+def exploration():
 
     principles=["PREOCCUPATION WITH FAILURE", "RELUCTANCE TO SIMPLIFY","COMMITMENT TO RESILIENCE",
       "DEFERENCE TO EXPERTISE", "SENSITIVITY TO OPERATION"] # TODO: tie this to the principles variable in model_pylist.py
 
     effects = model.get_results()
     entries = [dict(name=principle, value=effect) for principle, effect in zip (principles, effects)]
-    return render_template('layout.html', entries=entries)
+    return render_template('exploration.html', entries=entries)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -30,7 +30,7 @@ def submit():
     """
     print(request.form)
     model.input_intervention('Stop-Work-Authority', request.form['value'])
-    return redirect(url_for('layout'))
+    return redirect(url_for('exploration'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

c25265e2d01f5a6f3c89a20584dec4083af4e3a1
Yufei <yutao@pdx.edu>
2022-11-04 01:44:30 -0700

    final update for planning page structure

---
+++ js/methods.js
@@ -0,0 +1,16 @@
+function change() {
+    var button = document.getElementsByName("Save Strategy");
+    var buttonState = button.dataset.buttonState;
+    var buttonText;
+
+    if (buttonState === 'save') {
+        buttonText = "Saved";
+        buttonState = "saved";
+    } else {
+        buttonText = "Saved";
+        buttonState = "saved"
+    }
+
+    button.innerHTML = buttonText;
+    button.dataset.buttonState = buttonState;
+}
